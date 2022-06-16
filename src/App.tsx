924278e0031e2cbb0321d2cc7c5a124cbaa08de3
Alexandr Shishov <ernesto.chrome@mail.ru>
2022-06-16 21:56:10 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -50,6 +50,11 @@ function App() {
                 removeTask={removeTask}
                 changeFilter={changeFilter}
                 addTask={addTask} />
+            <Todolist title='What to learn'
+                tasks={tasksForTodolist}
+                removeTask={removeTask}
+                changeFilter={changeFilter}
+                addTask={addTask} />
         </div >
     );
 }
