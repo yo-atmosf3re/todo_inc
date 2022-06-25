abcd8b4f2fe047bffcece2221c25f74e4e43102c
Alexandr Shishov <ernesto.chrome@mail.ru>
2022-06-25 23:23:38 +0300

    upd

--- src/App.tsx
+++ src/App.tsx
@@ -60,6 +60,13 @@ function App() {
                 addTask={addTask}
                 changeTaskStatus={changeStatus}
                 filter={filter} />
+            <Todolist title='What to learn'
+                tasks={tasksForTodolist}
+                removeTask={removeTask}
+                changeFilter={changeFilter}
+                addTask={addTask}
+                changeTaskStatus={changeStatus}
+                filter={filter} />
         </div >
     );
 }
