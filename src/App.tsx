0fe8e7e10ccd2fed326862344dfe93f75d982a2b
Alexandr Shishov <ernesto.chrome@mail.ru>
2022-07-02 22:10:54 +0300

    upd

---
+++ src/App.tsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import { v1 } from 'uuid';
+import './App.css';
+import { TaskType, Todolist } from './Todolist';
+
+export type FilterValuesType = 'all' | 'complited' | 'active';
+
+function App() {
+    let [tasks, setTasks] = useState<Array<TaskType>>([
+        { id: v1(), title: "HTML&CSS", isDone: true },
+        { id: v1(), title: "JS", isDone: true },
+        { id: v1(), title: "ReactJS", isDone: false },
+        { id: v1(), title: "Redux", isDone: false },
+        { id: v1(), title: "GraphQL", isDone: false },
+    ]);
+
+    let [filter, setFilter] = useState<FilterValuesType>('all');
+
+    function removeTask(id: string) {
+        let filteredTasks = tasks.filter(t => t.id !== id)
+        setTasks(filteredTasks);
+    }
+
+    function addTask(title: string) {
+        let newTask = {
+            id: v1(),
+            title: title,
+            isDone: false
+        };
+        let newTasks = [newTask, ...tasks];
+        setTasks(newTasks);
+    }
+
+    function changeStatus(taskId: string, isDone: boolean) {
+        let task = tasks.find(t => t.id === taskId)
+        if (task) {
+            task.isDone = isDone;
+        }
+        setTasks([...tasks]);
+    }
+
+    function changeFilter(value: FilterValuesType) {
+        setFilter(value);
+    }
+
+    let tasksForTodolist = tasks;
+    if (filter === 'complited') {
+        tasksForTodolist = tasks.filter(t => t.isDone === true);
+    }
+    if (filter === 'active') {
+        tasksForTodolist = tasks.filter(t => t.isDone === false);
+    }
+
+    return (
+        <div className="App" >
+            <Todolist title='What to learn'
+                tasks={tasksForTodolist}
+                removeTask={removeTask}
+                changeFilter={changeFilter}
+                addTask={addTask}
+                changeTaskStatus={changeStatus}
+                filter={filter} />
+        </div >
+    );
+}
+
+export default App;
