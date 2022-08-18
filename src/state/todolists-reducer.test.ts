e29ff4b8ebb0f42a921e2ddd1a026ecabbf9941e
Alexandr Shishov <ernesto.chrome@mail.ru>
2022-08-18 11:07:10 +0300

    created a reducer for the TodoList component; created tests for the new reducer;

---
+++ src/state/todolists-reducer.test.ts
@@ -0,0 +1,37 @@
+import { TodolistTypes } from './../App';
+import { todolistsReducer } from './todolists-reducer';
+import { v1 } from 'uuid';
+
+test.skip('correct todolist should be removed', () => {
+   let todolistId1 = v1();
+   let todolistId2 = v1();
+
+   const startState: Array<TodolistTypes> = [
+      { id: todolistId1, title: "What to learn", filter: "all" },
+      { id: todolistId2, title: "What to buy", filter: "all" }
+   ]
+
+   const endState = todolistsReducer(startState, { type: 'REMOVE-TODOLIST', id: todolistId1 })
+
+   expect(endState.length).toBe(1);
+   expect(endState[0].id).toBe(todolistId2);
+});
+
+test('correct todolist should be added', () => {
+   let todolistId1 = v1();
+   let todolistId2 = v1();
+
+   let newTodolistTitle = "New Todolist";
+
+   const startState: Array<TodolistTypes> = [
+      { id: todolistId1, title: "What to learn", filter: "all" },
+      { id: todolistId2, title: "What to buy", filter: "all" }
+   ]
+
+   const endState = todolistsReducer(startState, { type: 'ADD-TODOLIST', title: newTodolistTitle })
+
+   expect(endState.length).toBe(3);
+   expect(endState[2].title).toBe(newTodolistTitle);
+   expect(endState.filter).toBe('all')
+});
+
--- src/state/user-reducer.ts
+++ src/state/user-reducer.ts
@@ -9,14 +9,23 @@ export type ActionType = {
    [key: string]: any
 }
 
-export const userReducer = (state: StateType, action: ActionType): any => {
+export const userReducer = (state: StateType, action: ActionType): StateType => {
    switch (action.type) {
       case 'INCREMENT-AGE':
-         state.age = state.age + 1;
-         return state;
+         return {
+            ...state,
+            age: state.age + 1,
+         }
       case 'INCREMENT-CHILDREN-COUNT':
-         state.childrenCount = state.childrenCount + 1;
-         return state;
+         return {
+            ...state,
+            childrenCount: state.childrenCount + 1,
+         }
+      case 'CHANGE-NAME':
+         return {
+            ...state,
+            name: action.newName
+         }
       default:
          throw new Error("I don't understand this type")
    }
