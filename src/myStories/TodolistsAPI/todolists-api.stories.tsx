7a40dc9aea543bb367ce82c80050357d37e56e25
Alexandr Shishov <ernesto.chrome@mail.ru>
2022-09-20 22:36:59 +0300

    updating the logic in stories, which is responsible for post, delete and put requests;

--- src/myStories/TodolistsAPI/todolists-api.stories.tsx
+++ src/myStories/TodolistsAPI/todolists-api.stories.tsx
@@ -6,7 +6,10 @@ export default {
 }
 
 const settings = {
-   withCredentials: true
+   withCredentials: true,
+   headers: {
+      "API-KEY": "4e3f5b5e-2baf-457b-9c14-722f665306af"
+   }
 }
 
 export const GetTodolists = () => {
@@ -17,16 +20,18 @@ export const GetTodolists = () => {
             setState(res.data)
          })
    }, [])
-
-   // *
-   // * 55:29
-   // *
-
    return <div> {JSON.stringify(state)}</div>
 }
 export const CreateTodolist = () => {
    const [state, setState] = useState<any>(null)
    useEffect(() => {
+      axios.post('https://social-network.samuraijs.com/api/1.1/todo-lists', {
+         title: 'Test todolist'
+      }, settings)
+         .then((res) => {
+            debugger
+            setState(res.data)
+         })
    }, [])
 
    return <div> {JSON.stringify(state)}</div>
@@ -34,6 +39,10 @@ export const CreateTodolist = () => {
 export const DeleteTodolist = () => {
    const [state, setState] = useState<any>(null)
    useEffect(() => {
+      axios.delete('https://social-network.samuraijs.com/api/1.1/todo-lists/742d37f2-2d27-4daf-869d-cc993642c56e', settings)
+         .then((res) => {
+            setState(res.data)
+         })
    }, [])
 
    return <div> {JSON.stringify(state)}</div>
@@ -41,6 +50,13 @@ export const DeleteTodolist = () => {
 export const UpdateTodolistTitle = () => {
    const [state, setState] = useState<any>(null)
    useEffect(() => {
+      axios.post('https://social-network.samuraijs.com/api/1.1/todo-lists', {
+         title: 'Test todolist'
+      }, settings)
+         .then((res) => {
+            debugger
+            setState(res.data)
+         })
    }, [])
 
    return <div> {JSON.stringify(state)}</div>
