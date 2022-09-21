a85a4c8bc010e43e48119c3f16aa965d082a2d77
Alexandr Shishov <ernesto.chrome@mail.ru>
2022-09-21 22:01:41 +0300

    adding typing for each type of request in the form of a clarification (generic);

--- src/api/todolists-API.tsx
+++ src/api/todolists-API.tsx
@@ -13,10 +13,33 @@ export type TodolistType = {
    addedDate: string
    orded: number
 }
+type CreateTodolistResponseType = {
+   resultCode: number
+   messages: Array<string>
+   fieldsErrors: Array<string>
+   data: {
+      item: TodolistType
+   }
+}
+type DeleteTodolistResponseType = {
+   resultCode: number
+   messages: Array<string>
+   fieldsErrors: Array<string>
+   data: {}
+}
+type UpdateTodolistResponseType = {
+   resultCode: number
+   messages: Array<string>
+   fieldsErrors: Array<string>
+   data: {}
+}
+
+
+
 
 export const todolistsAPI = {
    getTodolists: () => axios.get<Array<TodolistType>>('https://social-network.samuraijs.com/api/1.1/todo-lists', settings),
-   createTodolist: (title: string) => axios.post('https://social-network.samuraijs.com/api/1.1/todo-lists', { title }, settings),
-   deleteTodolist: (id: string) => axios.delete(`https://social-network.samuraijs.com/api/1.1/todo-lists/${id}`, settings),
-   updateTodolist: (id: string, title: string) => axios.put(`https://social-network.samuraijs.com/api/1.1/todo-lists/${id}`, { title }, settings),
+   createTodolist: (title: string) => axios.post<CreateTodolistResponseType>('https://social-network.samuraijs.com/api/1.1/todo-lists', { title }, settings),
+   deleteTodolist: (id: string) => axios.delete<DeleteTodolistResponseType>(`https://social-network.samuraijs.com/api/1.1/todo-lists/${id}`, settings),
+   updateTodolist: (id: string, title: string) => axios.put<UpdateTodolistResponseType>(`https://social-network.samuraijs.com/api/1.1/todo-lists/${id}`, { title }, settings),
 }
